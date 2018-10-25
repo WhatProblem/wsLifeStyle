6d984d6d146877099ef4aa4ed5d9dfe6307b2fea
WhatProblem <18355350915@163.com>
2018-10-25 21:07:25 +0800

    图片上传提交

---
+++ pages/components/goodDesc/goodDesc.js
@@ -0,0 +1,23 @@
+// pages/components/goodDesc/goodDesc.js
+Component({
+  options: {
+    multipleSlots: true
+  },
+
+  properties: {
+
+  },
+
+  data: {
+    imgUrl: [
+      { src: '/assets/image/detail_model.jpg' },
+      { src: '/assets/image/detail_info.jpg' },
+      { src: '/assets/image/detail_color.jpg' },
+      { src: '/assets/image/detail_sizetip.jpg' },
+      { src: '/assets/image/detail_size.jpg' }
+    ]
+  },
+
+  methods: {
+  }
+})
